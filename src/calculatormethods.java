68589922a100c9202bae0f1aa3e83ea4548a2cc3
Faheem <fame2845@gmail.com>
2021-05-18 17:37:54 -0400

    commited calculator

---
+++ src/calculatormethods.java
@@ -0,0 +1,29 @@
+
+public class calculatormethods {
+	public static void main(String[] args) {
+	System.out.println(add());
+	System.out.println(sub());
+	System.out.println(mul());
+	System.out.println(div());
+	}
+public static int add() {
+	int x=20,y=10,z;
+	z=x+y;
+	return z;
+}
+public static int sub() {
+	int a=30,b=20,c;
+	c=a+b;
+	return c;
+}
+public static int mul() {
+	int a1=20,b1=10,c1;
+	c1=a1+b1;
+	return c1;
+}
+public static int  div() {
+	int a2=10,b2=5,c2;
+	c2=a2+b2;
+	return c2;
+}
+}
